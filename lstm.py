0272ec6c5ecf89fae211ed5a6759635519f7d244
johncava <jcava@asu.edu>
2018-04-21 01:36:47 +0000

    loaded sequence datasets into lstms for training

--- lstm.py
+++ lstm.py
@@ -3,26 +3,32 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import Variable
 from utility import *
 
 # load data
 data = create_dataset()
 
-lstm = nn.LSTM(3, 3)  # Input dim is 3, output dim is 3
+lstm = nn.LSTM(22, 9)  # Input dim is 3, output dim is 3
+
+'''
 inputs = [autograd.Variable(torch.randn((1,3)))
           for _ in range(5)]  # make a sequence of length 5
 data_y = [[0,0,1],[0,1,0],[1,0,0],[0,1,0],[0,0,1]]
 outputs =[autograd.Variable(torch.Tensor(y)).view(1,1,3)
 	  for y in data_y]
-
+'''
 #print inputs
 #print outputs
 loss_function = nn.MSELoss()
 optimizer = optim.SGD(lstm.parameters(), lr=0.1)
 
 # initialize the hidden state.
-hidden = (autograd.Variable(torch.randn(1, 1, 3)),
-          autograd.Variable(torch.randn((1, 1, 3))))
+hidden = (autograd.Variable(torch.randn(1, 1, 9)),
+          autograd.Variable(torch.randn((1, 1, 9))))
+
+inputs = [Variable(torch.Tensor(x)) for x in data[0][0]]
+outputs = [Variable(torch.Tensor(y)).view(1,1,9) for y in data[0][1]]
 
 for epoch in xrange(10):
 	l = 0
